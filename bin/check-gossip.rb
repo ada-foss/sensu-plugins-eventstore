f28d23b03b7ded45bb80fb0348f0a813757a30c9
Berin Smaldon <berin.smaldon1@nhs.net>
2017-08-08 08:30:37 +0100

    update to check epoch position

--- bin/check-gossip.rb
+++ bin/check-gossip.rb
@@ -93,6 +93,30 @@ class CheckGossip < Sensu::Plugin::Check::CLI
     document.xpath '//State'
   end
 
+  def get_master_epoch(document)
+    xml_nodes = document.xpath "/ClusterInfoDto/Members/MemberInfoDto[State='Master']/EpochPosition"
+    # assume that one and only one node is master, this is being actively checked elsewhere so we would
+    # already know about it if this weren't the case
+    xml_nodes[0].content
+  end
+
+  def get_target_ip(document)
+    xml_nodes = document.xpath "/ClusterInfoDto/ServerIp"
+    xml_nodes[0].content
+  end
+
+  def get_target_epoch(document)
+    target_ip = get_target_ip document
+    # expect this to return one and only one node, if not then should fall over and die
+    xml_nodes = document.xpath "/ClusterInfoDto/Members/MemberInfoDto[InternalHttpIp='#{target_ip}']/EpochPosition"
+
+    if xml_nodes.count == 1
+      xml_nodes[0].content
+    else
+      critical_malformed_gossip "number of nodes matching InternalHttpIp == #{xml_nodes.count}"
+    end
+  end
+
   def only_one_master?(document)
     get_master_count(document) == 1
   end
@@ -111,6 +135,10 @@ class CheckGossip < Sensu::Plugin::Check::CLI
     nodes.all? { |node| node_is_alive? node }
   end
 
+  def target_epoch_up_to_date?(document)
+    get_target_epoch(document) == get_master_epoch(document)
+  end
+
   def critical_missing_nodes(xml_doc, expected_nodes)
     critical "Wrong number of nodes, was #{get_members(xml_doc).count} should be #{expected_nodes}"
   end
@@ -119,6 +147,12 @@ class CheckGossip < Sensu::Plugin::Check::CLI
   end
   def critical_master_count(xml_doc)
     critical "Wrong number of node masters, there should be 1 but there were #{get_master_count(xml_doc)} masters"
+  end
+  def critical_target_behind_master(xml_doc)
+    critical "Target epoch [#{get_target_epoch(xml_doc)}] is behind master epoch [#{get_master_epoch(xml_doc)}]"
+  end
+  def critical_malformed_gossip(reason)
+    critical "Malformed gossip file, because of: #{reason}"
   end
   def warn_nodes_not_ready(xml_doc)
     states = get_states xml_doc
@@ -155,6 +189,9 @@ class CheckGossip < Sensu::Plugin::Check::CLI
     puts "Checking node state"
     warn_nodes_not_ready xml_doc unless all_nodes_master_or_slave? xml_doc
 
-    ok "#{gossip_address} is gossiping with #{expected_nodes} nodes, all nodes are alive, exactly one master node was found and all other nodes are in the 'Slave' state."
+    puts "Checking that target epoch is at same position as master"
+    critical_target_behind_master xml_doc unless target_epoch_up_to_date? xml_doc
+
+    ok "#{gossip_address} is gossiping with #{expected_nodes} nodes, all nodes are alive, exactly one master node was found, all other nodes are in the 'Slave' state, and all nodes are up to date."
   end
 end
